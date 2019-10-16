26373f3bb16dcf9d5c44366284b5e792b1153007
Ревельская Анна <s22e_revelskaya@179.lan>
2019-10-16 16:47:26 +0300

    Saving from repeats

---
+++ 12345.pas
@@ -0,0 +1,13 @@
+﻿var prq,q:integer; //защита от повторений
+begin
+ prq:=1;
+ for var i:=1 to 10 do
+ begin
+   repeat
+   q:=random(1,5);
+   until q<>prq;
+   prq:=q;
+   write(q,' ');
+ end;
+ 
+end.
